21593013952a45c86940ad4e40b5e854e440e846
Jimmy <acsd68500@gmail.com>
2014-10-21 12:25:56 +0800

    correct ex3

--- basic/basic-ex3.rb
+++ basic/basic-ex3.rb
@@ -1,6 +1,6 @@
-movies = { :"1999" => "cat and dog", 
-		   :"2010" => "after tomorrow", 
-		   :"2014" => "等一人咖啡"}
+movies = {the_dog: 2000,
+		  the_cat: 2001,
+		  the_fish: 2002}
 movies.each do |movie|
 	puts movie
 end
