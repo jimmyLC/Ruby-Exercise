088f9d87f78e330c746f0e70326ba0c0bf5f356f
Jimmy <acsd68500@gmail.com>
2014-10-22 20:52:11 +0800

    ex2 done

--- object2/object2-ex2.rb
+++ object2/object2-ex2.rb
@@ -1,7 +1,7 @@
 class MyCar 
 
 	attr_accessor :color
-	attr_reader :year
+	attr_reader :year, :model
 
 	def self.gas_mileage(gas, mileage)
 		"#{mileage/gas} miles per gallon of gas"
@@ -32,11 +32,16 @@ class MyCar
 		@current_speed = 0
 		self.current_speed
 	end
-	
+
 	def spray_paint(color)
 		self.color = color # 結果輸出和@color, 那應該差別在哪？ 應該優先用哪個？
 		puts "your car color is #{color}"
 	end
+
+	def to_s
+		"your car is #{model}, #{color}, #{year} awsome!"
+	end
 end
 
-p MyCar.gas_mileage(10, 100)
+car = MyCar.new("2011", "A8", "white")
+puts car
